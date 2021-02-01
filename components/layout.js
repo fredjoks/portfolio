bb494fe6b90f056d79f196510c39bdb064c0388d
Fred Jõks <fredjox@gmail.com>
2021-02-01 18:28:42 +0200

    fix: removed broken css

--- components/layout.js
+++ components/layout.js
@@ -55,7 +55,7 @@ export default function Layout({ children, home }) {
                 </a>
               </Link>
               <div className={styles.headerCredentials}>
-                <h2 className>
+                <h2>
                   <Link href="/">
                     <a className={utilStyles.colorInherit}>{name}</a>
                   </Link>
